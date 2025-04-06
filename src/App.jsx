622caab6fc3b2cdd3d193f2cfec3bbc8da8ef542
Vikas <133359828+Vikask2612@users.noreply.github.com>
2025-04-06 11:59:26 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,53 @@
+// useState hooks
+
 // as
-// definition of hooks: special types of functions 
-import React from 'react'
-import { useState } from 'react'
 
-const App = () => {
-    const [counter, setCounter] = useState(0)
+// import React,{useState} from 'react'
 
-    function addValue(){
-      setCounter(counter+1)
-    }
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
 
-    function removeValue(){
-      setCounter(counter-1)
-    }
+//   const Increment=(e)=>{
+//       // console.log(e.target.value);
+//       setCounter(counter+1);
+//   }
 
-  return (
-    <div>
-        <h1>Chai aur react</h1>
-        <h2>Current value is: {counter}</h2>
+//   return (
+//     <>
+//     <h2 className='pl-4 py-2 px-3 border-black border-2 rounded-2xl bg-amber-400 text-black'> {counter}</h2>
+   
+//     <button onClick={Increment} 
+//     >Increment</button>
+//     </>
+//   )
+// }
 
-        <button onClick={addValue}>Add value</button>
-        <button onClick={removeValue}>Remove value</button>
-    </div>
-  )
-}
+// export default App
 
-export default App
+// ------------------------------------------------------------------------------------------------------------
+
+// useEffect Hooks
+
+// import React,{useEffect, useState} from 'react'
+// import axios from 'axios'
+
+// const App = () => {
+//   const[data ,setData]=useState('');
+
+//   useEffect(()=>{
+//       axios.get("https://jsonplaceholder.typicode.com/comments")
+//         .then((response)=>{
+//         setData(response.data[498].email);
+//         console.log("API was called");
+//     });
+//   });
+  
+//   return (
+//     <div className='bg-black text-white text-3xl'>Hello{data}</div>
+//   )
+// }
+
+// export default App
+
+// ------------------------------------------------------------------------------------------
+
